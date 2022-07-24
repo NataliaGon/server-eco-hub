c6b371600586953d0d34f1f123a0759ffe1fd288
Natalia <gon4arovanatalija@gmail.com>
2022-07-24 13:14:52 +0300

    +

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json({ limit: "50mb" }));
 app.enable("trust proxy");
 
 const allowedOrigins = ['http://localhost:3000',
-                      'https://eco-hub.herokuapp.com'];
+                      'http://eco-hub.herokuapp.com'];
 app.use(cors({
   origin: function(origin, callback){
     // allow requests with no origin 
